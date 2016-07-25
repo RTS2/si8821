7104e9c0b384119fd6115b12fd6111408a6f7747
rts2-user <rts2@tbt-control.tbt>
2016-07-25 05:17:16 +0200

    removed extra files

--- module/.si8821.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/petr/SI/si8821/kernel/si8821.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/petr/SI/si8821/kernel/si8821.ko /home/petr/SI/si8821/kernel/si8821.o /home/petr/SI/si8821/kernel/si8821.mod.o
